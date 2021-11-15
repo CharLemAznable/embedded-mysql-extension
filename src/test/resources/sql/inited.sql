f456c95b500f7fdfcec017b926b826c8c26dd6e3
CharLemAznable <545541819@qq.com>
2021-11-15 20:23:40 +0800

    Fixed test sql.

--- src/test/resources/sql/inited.sql
+++ src/test/resources/sql/inited.sql
@@ -1,10 +1,10 @@
 -- noinspection SqlDialectInspectionForFile
 -- noinspection SqlNoDataSourceInspectionForFile
 
-CREATE TABLE INITED_TEST (
-  A INT NOT NULL,
-  B CHAR(10)
+create table inited_test (
+  a int not null,
+  b char(10)
 );
 
-INSERT INTO INITED_TEST VALUES(1, '111');
-INSERT INTO INITED_TEST VALUES(2, '222');
+insert into inited_test values(1, '111');
+insert into inited_test values(2, '222');
